292f8d3d59d96ca2e7669fcf79337c57dc023cf5
siwar-zr <siwarzrelli180959@gmail.com>
2026-04-10 14:52:42 +0100

    notions de base poo

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,6 +1,4 @@
 
->
-src/Hello.java,2\7\278c8a0422c20651ff175b8b5b33f7256b33293c
 B
 src/Operators.java,9\5\9591547a47dfee877e84426519f1d3e18bd46832
 C
---
+++ src/poo/Demo.java
@@ -0,0 +1,62 @@
+package poo;
+
+class Calculator{
+    int num = 5;
+
+    //Surcharge des methodes (method overloading)
+    public int add(int a, int b){
+
+        return a+b;
+    }
+    public int add(int a, int b, int c){
+
+        return a+b+c;
+    }
+    public double add(double a, double b){
+        return a+b;
+    }
+}
+
+/*class Computer{
+    public void playMusic(){
+        System.out.println("Music playing..");
+    }
+
+    public String getMeSmthg(int cost){
+        if (cost >= 10){
+            return "Something";
+        }
+        return "Nothing";
+
+
+    }
+}*/
+public class Demo {
+    public static void main(String[] args) {
+        /*int num1 = 4;
+        int num2 = 5;*/
+
+        int data = 10;
+        Calculator obj = new Calculator();
+        int result = obj.add(2,5);
+        //System.out.println(result);
+        //System.out.println(obj);
+
+        Calculator obj1 = new Calculator();
+        obj.num = 8;
+
+        System.out.println(obj.num);
+        System.out.println(obj1.num);
+
+
+
+
+        /*int result = num1+num2;
+
+        System.out.println(result);*/
+
+        /*Computer obj1 = new Computer();
+        obj1.playMusic();
+        System.out.println(obj1.getMeSmthg(1));*/
+    }
+}
